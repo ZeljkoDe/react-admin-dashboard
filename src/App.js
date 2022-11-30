29ea9f2715f2cc2a402877f65debaeec53f821e6
Zeljko Damnjanovic <z.damnjanovic@emakina.rs>
2022-11-30 07:49:37 +0100

    Router Setup

--- src/App.js
+++ src/App.js
@@ -1,3 +1,25 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Home, List, Login, New, Single } from './pages';
+
 export default function App() {
-	return <div>App</div>;
+	return (
+		<>
+			<BrowserRouter>
+				<Routes>
+					<Route path='/' element={<Home />} />
+					<Route path='/login' element={<Login />} />
+					<Route path='/users'>
+						<Route index element={<List />} />
+						<Route path=':userId' element={<Single />} />
+						<Route path='new' element={<New />} />
+					</Route>
+					<Route path='/products'>
+						<Route index element={<List />} />
+						<Route path=':productID' element={<Single />} />
+						<Route path='new' element={<New />} />
+					</Route>
+				</Routes>
+			</BrowserRouter>
+		</>
+	);
 }
